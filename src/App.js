a0aeb9d76a29980e771f9777a1ece0638c416428
mitalitagadiya <email@example.com>
2022-06-10 15:47:33 +0530

    10

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header/Header';
+import Footer from './Component/Footer/Footer';
+import Home from './Container/Home/Home';
+import Departments from './Container/Departments/Departments';
+import { Route } from 'react-router-dom';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      
+      <switch>
+        <Route path={"/"} exact component={Home}/>
+        <Route path={"/departments"} exact component={Departments}/>
+        
+      </switch>
+
+      <Footer />
+    </>
   );
 }
 
